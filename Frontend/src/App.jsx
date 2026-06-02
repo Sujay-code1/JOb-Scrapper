7001f703c242c76b5a5c2374443d3952ff1e98c9
Sujay-code1 <alexsujay111@gmail.com>
2026-06-02 22:10:49 +0530

    initial project structure completed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="bg-red-500 min-h-screen flex items-center justify-center">
+      <h1>Job Scrapper</h1>
+    </div>
+  )
+}
+
+export default App
